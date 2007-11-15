4dfcf3a8a07c7c022942175369aec0d2072d9586
gaitan <gaitan@39b0689c-e03e-0410-b2c8-7dcf72c758b3>
2007-11-15 23:31:54 +0000

    modelo simplificado, agregado editor wysiwyg basado en jquery

--- trunk/seguimiento/models.py
+++ trunk/seguimiento/models.py
@@ -1,87 +1,89 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Persona(models.Model):
-    SEXO_OPCIONES = (
-        ('M', 'Masculino'),
-        ('F', 'Femenino'),       
-    )
-    nombre = models.CharField(maxlength=50)
-    apellido = models.CharField(maxlength=50)
-    alias = models.SlugField(blank=True)
-    ocupacion = models.CharField(maxlength=50)
-    sexo = models.CharField(maxlength=1,choices=SEXO_OPCIONES, radio_admin=True)
-    observaciones = models.TextField(blank=True)
+
     def __unicode__(self):
         return self.nombre + ' ' + self.apellido
-
+        
 	class Admin:
 		pass
 
-
-class Abogado(Persona):
+    nombre = models.CharField(maxlength=50)
+    apellido = models.CharField(maxlength=50)
+    alias = models.SlugField(blank=True)
+    sexo = models.CharField(maxlength=1,choices=(('M', 'Masculino'),('F', 'Femenino'),), radio_admin=True)
+    observaciones = models.TextField(blank=True)
     ocupacion = models.CharField(maxlength=50, default='Abogado', editable=False)
 
-    def lala(self):
-        pass
+class Abogado(models.Model):
+    nombre = models.CharField(maxlength=50)
+    apellido = models.CharField(maxlength=50)
+    observaciones = models.TextField(blank=True)
+    ocupacion = models.CharField(maxlength=50, default='Abogado', editable=False)
+
+    def __unicode__(self):
+        return self.nombre + ' ' + self.apellido
     
     class Admin:
         pass
     
-class Juez(Persona):
-    ocupacion = models.CharField(maxlength=50, default='Juez', editable=False)
-    def lala(self):
-        pass
-    class Admin:
-        pass
-        
-class Fiscal(Persona):
-    ocupacion = models.CharField(maxlength=50, default='Fiscal', editable=False)
+       
+class Fiscal(models.Model):
+    nombre = models.CharField(maxlength=50)
+    apellido = models.CharField(maxlength=50)
+    observaciones = models.TextField(blank=True)
     fiscalia = models.CharField(maxlength=100)
     direccion_fiscalia =  models.CharField(maxlength=150)
     telefonos = models.SlugField(blank=True)
 
-    def lala(self):
-        pass
+    def __unicode__(self):
+        return self.nombre + ' ' + self.apellido
             
     class Admin:
         pass
 
-
-class Querellante(Persona):
+class Querellante(models.Model):
+    nombre = models.CharField(maxlength=50)
+    apellido = models.CharField(maxlength=50, blank=True)
+    fisicaOjuridica = models.CharField(maxlength=1,choices=(('F', u'Física'),('J', u'Jurídica'),))
+    observaciones = models.TextField(blank=True)
     abogado = models.ManyToManyField(Abogado)
     def __unicode__(self):
-        return "querellante: " + self.nombre + ' ' + self.apellido
-
+        return self.nombre + ' ' + self.apellido
     class Admin:
         pass
 
-
-
-class Joven(Persona):
-    madre = models.CharField("Nombre de la madre biologica", maxlength=100,blank=True)
-    padre = models.CharField("Nombre del padre biologico", maxlength=100,blank=True)
+class Joven(models.Model):
+    nombre = models.CharField(maxlength=50)
+    apellido = models.CharField(maxlength=50, blank=True)
+    madre = models.CharField(u'Nombre de la madre biológica', maxlength=100,blank=True)
+    padre = models.CharField(u'Nombre del padre biologico', maxlength=100,blank=True)
+    observaciones = models.TextField(blank=True)    
     nombreAprop = models.CharField("Nombre impuesto", maxlength=50,blank=True)
     fechaAprop = models.DateField("Fecha de apropiacion",blank=True)
     tipo = models.CharField("Situacion", maxlength=50, choices=(('APROP', 'Apropiado'),('ADOPT', 'Adoptado'),), radio_admin=True)
+
     def __unicode__(self):
         return self.nombre + ' ' + self.apellido
 
     class Admin:
         pass
     
-class Imputado(Persona):
-    tipoImputacion = models.CharField("Tipo de imputacion", maxlength=50, choices=(('Apropiacion', 'Apropiacion'),('Participe', 'Participe Necesario'),('Otro', 'Otro')))
-    rolImputacion = models.CharField("Rol de imputacion", maxlength=50, choices=(('Apropiador', 'Apropiador'),('Medico', 'Medico'),('Enfermera', 'Enfermera')))
-    mayor70 = models.BooleanField("Mayor de 70?")
-    procesado = models.BooleanField("Esta procesado?")
+class Imputado(models.Model):
+    nombre = models.CharField(maxlength=50)
+    apellido = models.CharField(maxlength=50, blank=True)
+    tipoImputacion = models.CharField("Tipo de imputacion", maxlength=50, choices=(('apropiacion', u'Apropiación'),('participacion', u'Participación'),('Otro', 'Otro')))
+    rolImputacion = models.CharField("Rol de imputacion", maxlength=50, choices=(('apropiador', 'Apropiador'),('medico', u'Médico'),('Enfermera', u'Enfermera')))
+    mayor70 = models.BooleanField(u'Mayor de 70 años?')
+    procesado = models.BooleanField(u'¿Está procesado?')
     def __unicode__(self):
-        return "imputado: " + self.nombre + ' ' + self.apellido
+        return self.nombre + ' ' + self.apellido
 
     
-
     class Admin:
         pass
 
@@ -89,7 +91,7 @@ class Imputado(Persona):
 
 
 class Analisis(models.Model):
-    joven = models.OneToOneField(Joven)
+    joven = models.ForeignKey(Joven)
     tipo = models.CharField(maxlength=10,choices=(('ADN', 'ADN'),('Sangre', 'Sangre'),) )
     fecha_realizacion = models.DateField(blank=True)
     resultado = models.TextField(blank=True)
@@ -101,17 +103,15 @@ class Analisis(models.Model):
         pass
 
 class EtapaJudicial(models.Model):
-    instancia = models.CharField(maxlength=50,choices=(('Juzgado', 'Juzgado'),('Tribunal', 'Tribunal'),) )
-    tipo_fuero = models.CharField(maxlength=50,choices=(('Penal', 'Penal'),('Apelacion', 'Apelacion'),) )
-    numero = models.CharField(maxlength=10)
+    nombre = models.CharField(maxlength=100)
+    tipoFuero = models.CharField(maxlength=50,choices=(('Penal', 'Penal'),('Apelacion', u'Apelacion'),) )
     ciudad = models.CharField(maxlength=50)
-    conocido_como = models.CharField(maxlength=50,blank=True)
     direccion = models.CharField(maxlength=50,blank=True)
     telefonos = models.SlugField(blank=True)
-    juez = models.ForeignKey(Juez)
+    personaContacto = models.CharField(maxlength=100,blank=True)
 
     def __unicode__(self):
-   	    return instancia + ' ' + tipo_fuero + ' ' + numero + ' de ' + ciudad
+   	    return self.nombre + ' de ' + self.ciudad
 
     class Admin:
         pass
@@ -121,22 +121,24 @@ class EtapaJudicial(models.Model):
 class Causa(models.Model):
 	caso = models.CharField(maxlength=100)
    	caratula = models.CharField(maxlength=100,blank=True)
-   	joven = models.ManyToManyField(Joven,blank=True)
-   	etapaJudicial = models.OneToOneField(EtapaJudicial)
+   	joven = models.ManyToManyField(Joven,verbose_name=u"Jóven relacionado/a con la causa",blank=True)
+   	etapaJudicial = models.ForeignKey(EtapaJudicial)
    	fiscal = models.ForeignKey(Fiscal)
-   	imputados = models.ForeignKey(Imputado)
+   	imputados = models.ManyToManyField(Imputado, blank=True)
    	punteo = models.TextField("Resumen de la causa", help_text="Puede usar codigo HTML para formatear y linkear su texto")
-   	
-   	
    	#observaciones = models.TextField(blank=True)
-   	def __unicode__(self):
-   	    return self.caso   	    
 
+   	def __unicode__(self):
+   		return self.caso
+    
 	class Admin:
-		pass
+		js = ('/datos/js/jquery/jquery.js',
+			'/datos/js/wymeditor/jquery.wymeditor.js',
+			'/datos/js/admin_textarea.js')
+		
 
 class Incidente(models.Model):    
-    caso_padre = models.OneToOneField(Causa)
+    caso_padre = models.ForeignKey(Causa)
     caso = models.CharField(maxlength=100)
     caratula = models.CharField(maxlength=100, blank=True)
     etapaJudicial = models.ForeignKey(EtapaJudicial) 
@@ -165,11 +167,12 @@ class Seguimiento(models.Model):
 	creado_por = models.ForeignKey(User)
 	causa = models.ForeignKey(Causa)
 	#autor = models.ForeignKey(Usuario)
-	fecha_vencimiento = models.OneToOneField(Evento, blank=True)
+	vencimiento = models.ForeignKey(Evento, blank=True)
 	foja = models.CharField(maxlength=20)
+	titulo = models.CharField(maxlength=100, blank=True)
 	comentario = models.TextField()
 
-	def lala(self):
+	def __unicode__(self):
 		pass
 
 	class Admin:
@@ -182,7 +185,7 @@ class SeguimientoIncidente(models.Model):
 	creado_por = models.ForeignKey(User)
 	Incidente = models.ForeignKey(Incidente)
 	#autor = models.ForeignKey(Usuario)
-	fecha_vencimiento = models.OneToOneField(Evento, blank=True)
+	fecha_vencimiento = models.ForeignKey(Evento, blank=True)
 	foja = models.CharField(maxlength=20)
 	comentario = models.TextField()
 
--- trunk/urls.py
+++ trunk/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^datos/(.*)$','django.views.static.serve',
+{'document_root': 'datos/', 'show_indexes': True}),
+
 )
 
 
