23a54cab8e7dbd008fba0abeef4d6c55ba7cd368
gaitan <gaitan@39b0689c-e03e-0410-b2c8-7dcf72c758b3>
2007-11-16 06:32:40 +0000

    agregada data inicial

---
+++ trunk/seguimiento/sql/juzgado.sql
@@ -0,0 +1,3 @@
+INSERT INTO `seguimiento_juzgado` (`id`, `nombreJuzgado`, `ciudad`, `juez`, `direccion`, `telefonos`, `personaContacto`, `emailJuzgado`, `observaciones`) VALUES 
+(1, 'Tribunal Superior de Justicia', 'Buenos Aires', 'José O. Casás', 'Cerrito 760 ', '4370-8500', '', 'ceremonial@tsjbaires.gov.ar', 'http://www.tsjbaires.gov.ar/');
+
