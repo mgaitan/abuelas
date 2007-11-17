27203026cdd2914e90ec1b0c79bb15ed6e1c66a8
gaitan <gaitan@39b0689c-e03e-0410-b2c8-7dcf72c758b3>
2007-11-17 05:41:58 +0000

    simplificado el diagrama

---
+++ seguimiento/sql/TipoJuzgado.sql
@@ -0,0 +1,5 @@
+INSERT INTO `seguimiento_tipojuzgado` (`id`, `nombre`) VALUES 
+(1, 'Intrucción'),
+(2, 'Oral'),
+(3, 'Apelación'),
+(4, 'Corte Suprema');
--- tareas/models.py
+++ tareas/models.py
@@ -1,36 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
-from abuelas.seguimiento.models import Causa
+from abuelas.seguimiento.models import Caso
 
-class Prioridad(models.Model):
-    nombre_prioridad = models.CharField("Nombre", maxlength=40, unique=True)
-    orden = models.IntegerField()
-    color = models.CharField(maxlength=40, null=True, blank=True)
+class Proyecto(models.Model):
+    nombre = models.CharField(u'Nombre del proyecto', maxlength=255)
+    fechaInicio =  models.DateField(u'Fecha de inicio', auto_now=True)
+    caso = models.ForeignKey(Caso, null=True, blank=True, verbose_name="Proyecto relacionado al caso", help_text=u'Dejá el campo en blanco si se trata de otro tipo de proyecto.' )    
+    fechaCierre = models.DateField(u'Fecha de finalización', null=True, blank=True, help_text=u'Dejá este campo en blanco si el proyecto no está finalizado')
+    descripcion = models.TextField(blank=True)
 
-    def __str__(self):
-        return self.nombre_prioridad
-
-    class Meta:
-        ordering = ['orden']
-
+    def __unicode__(self):
+        return self.nombre
+        
     class Admin:
         pass
+    
 
 
 class Tarea(models.Model):
+    OPCIONES = ((1,u'Urgente'), (2,u'Importante'), (3,u'Normal'), (4,u'Tranqui'), (5,u'Cuando sea'))
     descripcion = models.CharField(maxlength=255)
-    asignado = models.ForeignKey(User)
-    priority = models.ForeignKey(Prioridad)    
-    vencimiento = models.DateTimeField(null=True, blank=True)
-    causa = models.ForeignKey(Causa, null=True, blank=True, verbose_name="Relacionada a la causa")
-    fechaCreacion = models.DateTimeField(auto_now_add=True)
-    completado = models.DateTimeField(null=True, blank=True)
+    asignado = models.ManyToManyField(User,verbose_name="Tarea asignada a", null=True, blank=True, help_text="Dejar en blanco para que cualquiera pueda ver las tareas del staff")
+    fechaCreacion = models.DateTimeField(auto_now_add=True)    
+    prioridad = models.SmallIntegerField(choices=OPCIONES)
+    proyecto = models.ForeignKey(Proyecto)
+    vencimiento = models.DateTimeField(null=True, blank=True, help_text="Si esta tarea tiene un vencimiento, indique la fecha y la hora")
+    completado = models.DateTimeField(null=True, blank=True, help_text=u'Dejá este campo en blanco si la tarea no está finalizada')
     comentario = models.TextField(null=True, blank=True)
 
-    def __str__(self):
-        return self.descripcion
+    def __unicode__(self):
+        return self.descripcion + "(Prioridad: " + OPCIONES[prioridad-1][1] + ")"
 
     class Admin:
-        pass
-
+		list_filter = ('prioridad', 'vencimiento', 'asignado', 'completado')
+		search_fields = ['@descripcion', '@comentario']
+		list_display = ('fechaCreacion','prioridad', 'proyecto', 'descripcion', 'completado')
 
