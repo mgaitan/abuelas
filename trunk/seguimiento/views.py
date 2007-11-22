a57cf28f76cbe91c75109efa470db0d05be92933
gaitan <gaitan@39b0689c-e03e-0410-b2c8-7dcf72c758b3>
2007-11-22 11:32:58 +0000

    agregada la vista de causas y muchos efectos jquery al template

--- trunk/seguimiento/views.py
+++ trunk/seguimiento/views.py
@@ -1,11 +1,45 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render_to_response
-from abuelas.seguimiento.models import Juzgado
+from django import newforms as forms
+from django.db.models import Q
+from abuelas.seguimiento.models import *
+
+#formularios
+
+FormSeguimiento = forms.form_for_model(Seguimiento)
+
 # Create your views here.
 
+def causas(request):
+    pagina = {}
+    pagina["causas"] = Caso.objects.all().order_by('nombre_caso')
+    pagina["titulo"] = "Listado de Causas"
+    return render_to_response('causas.html', pagina)    
+		
+	
+def causa_detalle(request, causa_id):
+    causa = Caso.objects.get(pk=causa_id)
+    seguimientos = Seguimiento.objects.filter(Q(causa=causa_id)).order_by('-fecha_ingreso')
+    
+    if request.method == 'POST':
+        form = FormSeguimiento(request.POST)
+        if form.is_valid():
+            instance = form.save()
+            instance.save()
+            
+            return HttpResponseRedirect('/causas/' + causa_id)
+    else:
+        form = FormSeguimiento()
+        
+    return render_to_response('detalle_causa.html', {'causa': causa, 'seguimientos': seguimientos, 'form': form})
+	
+	
+
 def juzgados(request):
-	juzgados = Juzgado.objects.all().order_by('nombre_juzgado')
-	return render_to_response('juzgados.html', {'lista_juzgados': juzgados})
+    pagina = {}
+    pagina["juzgados"] = Juzgado.objects.all().order_by('nombre_juzgado')
+    pagina["titulo"] = "Juzgados"
+    return render_to_response('juzgados.html', pagina)
 
 
 def juzgados_detalle(request, juzgado_id):
