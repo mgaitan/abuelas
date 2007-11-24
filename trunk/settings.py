f761e46dee1e4e43377d54756bc9bdea8b13ffba
gaitan <gaitan@39b0689c-e03e-0410-b2c8-7dcf72c758b3>
2007-11-24 09:29:36 +0000

    ordenando archivos

--- trunk/settings.py
+++ trunk/settings.py
@@ -10,13 +10,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'postgresql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'Beppo'             # Or path to database file if using sqlite3
-DATABASE_USER = 'beppo'        # Not used with sqlite3.
+#DATABASE_ENGINE = 'postgresql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'abuelas'             # Or path to database file if using sqlite3
+DATABASE_USER = 'root'        # Not used with sqlite3.
 #DATABASE_PASSWORD = 'pass'         # Not used with sqlite3.
-DATABASE_PASSWORD = 'beppo'         # Not used with sqlite3.
-DATABASE_HOST = 'cabbage'            # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''            # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
